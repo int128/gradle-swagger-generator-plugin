bd392649be3d31b8b0e682c4f4610a6b784579f4
TANIGUCHI Kousuke <tani19@uchida.co.jp>
2019-02-18 17:04:32 +0900

    Add acceptance-tests. These are copied from codegen-v3.
    but build is failed.
    Fixed dependencies to build.
    In order to generate APIUtil.java, it needs to add supportFiles to
    component.
    
    acceptance-test/openapi-v3/multiple-codegen-versions use both V2 and
    OpenAPI Generator.
    
    acceptance-test/openapi-v3/multiple-sources.
    
    Fix below.
    - generated config package is conflict. set configPackage in config.json
    - dependencies is fixed. copied from generated pom.xml
    - supportingFiles in components. APIUtil is required for compile.

---
+++ acceptance-test/openapi-v3/custom-class/buildSrc/src/main/groovy/generators/MySpring.groovy
@@ -0,0 +1,6 @@
+package generators
+
+import org.openapitools.codegen.languages.SpringCodegen
+
+class MySpring extends SpringCodegen {
+}
---
+++ acceptance-test/src/test/groovy/org/hidetake/gradle/swagger/generator/test/openapi3/MultipleCodegenVersionsSpec.groovy
@@ -0,0 +1,58 @@
+package org.hidetake.gradle.swagger.generator.test.openapi3
+
+import org.gradle.testkit.runner.GradleRunner
+import org.gradle.testkit.runner.TaskOutcome
+import spock.lang.Specification
+
+import static org.hidetake.gradle.swagger.generator.test.Fixture.cleanBuildDir
+
+class MultipleCodegenVersionsSpec extends Specification {
+
+    GradleRunner runner
+
+    def setup() {
+        runner = GradleRunner.create()
+            .withProjectDir(new File('./openapi-v3/multiple-codegen-versions'))
+            .withPluginClasspath()
+            .forwardOutput()
+        cleanBuildDir(runner)
+    }
+
+    def 'tasks should exist in the project'() {
+        given:
+        runner.withArguments('--stacktrace', 'tasks')
+
+        when:
+        def result = runner.build()
+
+        then:
+        result.output.contains('generateSwaggerCodePetstoreV2 -')
+        result.output.contains('generateSwaggerCodePetstoreV3 -')
+        result.output.contains('generateSwaggerUIPetstoreV2 -')
+        result.output.contains('generateSwaggerUIPetstoreV3 -')
+        result.output.contains('validateSwaggerPetstoreV2 -')
+        result.output.contains('validateSwaggerPetstoreV3 -')
+    }
+
+    def 'generateSwaggerCode task should generate code'() {
+        given:
+        runner.withArguments('--stacktrace', 'generateSwaggerCode')
+
+        when:
+        def result = runner.build()
+
+        then:
+        result.task(':generateSwaggerCodePetstoreV2').outcome == TaskOutcome.SUCCESS
+        new File(runner.projectDir, 'build/swagger-code-petstoreV2/src/main/java/example/v2/api/PetsApi.java').exists()
+        result.task(':generateSwaggerCodePetstoreV3').outcome == TaskOutcome.SUCCESS
+        new File(runner.projectDir, 'build/swagger-code-petstoreV3/src/main/java/example/v3/api/PetsApi.java').exists()
+
+        when:
+        def rerunResult = runner.build()
+
+        then:
+        rerunResult.task(':generateSwaggerCodePetstoreV2').outcome == TaskOutcome.UP_TO_DATE
+        rerunResult.task(':generateSwaggerCodePetstoreV3').outcome == TaskOutcome.UP_TO_DATE
+    }
+
+}
