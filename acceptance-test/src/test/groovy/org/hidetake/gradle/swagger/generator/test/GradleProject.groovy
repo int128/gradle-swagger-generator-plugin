c95033968e202e87c8587315ccc246fad1fd2f50
Hidetake Iwata <int128@gmail.com>
2021-12-22 09:32:00 +0900

    Show build failure in annotation

--- acceptance-test/src/test/groovy/org/hidetake/gradle/swagger/generator/test/GradleProject.groovy
+++ acceptance-test/src/test/groovy/org/hidetake/gradle/swagger/generator/test/GradleProject.groovy
@@ -2,6 +2,7 @@ package org.hidetake.gradle.swagger.generator.test
 
 import org.gradle.testkit.runner.BuildResult
 import org.gradle.testkit.runner.GradleRunner
+import org.gradle.testkit.runner.UnexpectedBuildFailure
 
 class GradleProject {
     final String path
@@ -12,16 +13,23 @@ class GradleProject {
 
     BuildResult execute(String taskName) {
         file('build').deleteDir()
-        executeWithoutClean(taskName)
+        executeInternal(taskName)
     }
 
     BuildResult executeWithoutClean(String taskName) {
-        GradleRunner.create()
-            .withProjectDir(new File('projects'))
-            .withPluginClasspath()
-            .forwardOutput()
-            .withArguments('-s', "$path:$taskName")
-            .build()
+        executeInternal(taskName)
+    }
+
+    private BuildResult executeInternal(String taskName) {
+        try {
+            GradleRunner.create()
+                .withProjectDir(new File('projects'))
+                .withPluginClasspath()
+                .withArguments('-s', "$path:$taskName")
+                .build()
+        } catch (UnexpectedBuildFailure e) {
+            throw new RuntimeException("Build failure: ${e.buildResult.output}", e)
+        }
     }
 
     String absolutePath(String name) {
