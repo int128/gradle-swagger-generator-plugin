eebddff8feef325aae06ba8951422547de82162d
Jendrik Johannes <jendrik.johannes@gmail.com>
2023-12-14 08:01:22 +0100

    Fix ResolveSwaggerTemplate for configuration cache

--- src/main/groovy/org/hidetake/gradle/swagger/generator/ResolveSwaggerTemplate.groovy
+++ src/main/groovy/org/hidetake/gradle/swagger/generator/ResolveSwaggerTemplate.groovy
@@ -1,19 +1,25 @@
 package org.hidetake.gradle.swagger.generator
 
+import org.gradle.api.file.ArchiveOperations
 import org.gradle.api.file.DuplicatesStrategy
 import org.gradle.api.tasks.Sync
+
+import javax.inject.Inject
 
 /**
  * A task to extract template files from a dependency.
  */
-class ResolveSwaggerTemplate extends Sync {
+abstract class ResolveSwaggerTemplate extends Sync {
+
+    @Inject
+    abstract ArchiveOperations getArchives();
 
     ResolveSwaggerTemplate() {
-        from {
-            project.configurations.swaggerTemplate.resolve().collect {
-                project.zipTree(it)
+        from(project.configurations.swaggerTemplate.elements.map {
+            it.collect { zipFile ->
+                archives.zipTree(zipFile)
             }
-        }
+        })
         into("${project.buildDir}/swagger-template")
         duplicatesStrategy = DuplicatesStrategy.INCLUDE
     }
